a1c712b77c846fde6af0a90bb8be48c7de9b05dc
BastianAsmussen <bastian@asmsusen.tech>
2023-10-11 14:52:56 +0200

    Create basic `MySQL` server handler.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
+mod util;
+
+use util::db::get_database_url;
+use util::db::get_pool;
+
 fn main() {
-    println!("Hello, world!");
+    // Establish database connection.
+    let Ok(url) = get_database_url() else {
+        panic!("Missing enviornment variables!");
+    };
+    let pool = get_pool(&url);
 }
