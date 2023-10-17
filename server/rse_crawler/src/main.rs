b1261f76d27f3fb208314d32317a9eba5cc35b0e
Bastian Asmussen <bastianasmussen@tutanota.com>
2023-10-17 17:28:01 +0200

    Removed unnecessary import.

--- server/rse_crawler/src/main.rs
+++ server/rse_crawler/src/main.rs
@@ -5,7 +5,6 @@ mod utils;
 use log::{error, info};
 use std::collections::HashMap;
 
-use crate::crawler::crawl;
 use crate::utils::seed_urls;
 use crate::utils::timer::{format_time, Timer};
 use spider::tokio;
@@ -32,75 +31,68 @@ async fn main() {
 
     let mut websites = Vec::new();
 
-    let chunks = seed_urls
-        .chunks(utils::get_worker_threads())
-        .map(|chunk| chunk.join("\n"));
-    for chunk in chunks {
-        let chunk_websites = match tokio::spawn(async move {
-            let mut websites = Vec::new();
-            for url in chunk.split('\n') {
-                let website = match crawl(url).await {
-                    Ok(website) => website,
-                    Err(e) => {
-                        error!("Failed to crawl seed URL {url}! (Error: {e})");
+    let crawl = async {
+        let chunks = seed_urls
+            .chunks(utils::get_worker_threads())
+            .map(|chunk| chunk.join("\n"));
+        for chunk in chunks {
+            let chunk_websites = match tokio::spawn(async move {
+                let mut websites = Vec::new();
+                for url in chunk.split('\n') {
+                    let website = match crawler::crawl(url).await {
+                        Ok(website) => website,
+                        Err(e) => {
+                            error!("Failed to crawl seed URL {url}! (Error: {e})");
 
-                        continue;
+                            continue;
+                        }
+                    };
+
+                    info!("Crawled seed URL {url}!");
+
+                    websites.push(website.clone());
+
+                    // Index crawled websites.
+                    info!("Indexing crawled websites...");
+
+                    let mut indexed_pages = HashMap::new();
+                    let pages = indexer::scrape(&website);
+                    for url in pages.keys() {
+                        info!("Indexed page {url}!");
+
+                        indexed_pages.insert(url.to_string(), website.clone());
+
+                        // TODO: Store pages in database.
+                        info!("{:#?}", pages.get(url).unwrap());
                     }
-                };
 
-                info!("Crawled seed URL {url}!");
+                    info!(
+                        "Indexed {} pages in {}!",
+                        indexed_pages.len(),
+                        format_time(&time)
+                    );
+                }
 
-                websites.push(website);
-            }
+                websites
+            })
+            .await
+            {
+                Ok(websites) => websites,
+                Err(e) => {
+                    error!("Failed to crawl seed URLs (Error: {e})");
 
-            websites
-        })
-        .await
-        {
-            Ok(websites) => websites,
-            Err(e) => {
-                error!("Failed to crawl seed URLs (Error: {e})");
+                    continue;
+                }
+            };
 
-                continue;
-            }
-        };
+            websites.extend(chunk_websites);
+        }
+    };
 
-        websites.extend(chunk_websites);
-    }
-
+    let (time, ()) = timer.time(|| crawl).await;
     info!(
         "Crawled {} seed URLs in {}!",
         websites.len(),
-        format_time(&timer.total_time())
-    );
-
-    // Index crawled websites.
-    info!("Indexing crawled websites...");
-
-    let (time, indexed_pages) = timer
-        .time(|| async {
-            info!("Indexing crawled websites...");
-
-            let mut indexed_pages = HashMap::new();
-            for website in websites {
-                let pages = indexer::scrape(&website);
-                for url in pages.keys() {
-                    info!("Indexed page {url}!");
-
-                    indexed_pages.insert(url.to_string(), website.clone());
-
-                    // TODO: Store pages in database.
-                    info!("Data: {:#?}", pages.get(url).unwrap());
-                }
-            }
-
-            indexed_pages
-        })
-        .await;
-
-    info!(
-        "Indexed {} pages in {}!",
-        indexed_pages.len(),
         format_time(&time)
     );
 }
