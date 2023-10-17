e5a1c4124cacff8d442000b9a5c2adbe19bd807c
BastianAsmussen <bastian@asmsusen.tech>
2023-10-17 14:24:21 +0200

    Create new crawler.
    Created a new crawler runner, can utilize multiple threads, specified by the `WORKER_THREADS` environment variable (defaults to number of logical CPUs in the system).

--- server/rse_crawler/src/main.rs
+++ server/rse_crawler/src/main.rs
@@ -1,98 +1,105 @@
+mod crawler;
+mod indexer;
 mod utils;
 
-use log::{error, info, warn};
+use log::{error, info};
+use std::collections::HashMap;
 
-use std::time::{Instant};
-
-use crate::utils::html::index_pages;
-
+use crate::crawler::crawl;
+use crate::utils::seed_urls;
+use crate::utils::timer::{format_time, Timer};
 use spider::tokio;
-use spider::website::Website;
-use utils::db_manager::{get_database_url, get_pool};
-use utils::seed_urls::get_seed_urls;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
+    let mut timer = Timer::default();
+
     // Fetch seed URLs.
     info!("Fetching seed URLs...");
-
-    let Some(seed_urls) = get_seed_urls() else {
-        panic!("Failed to fetch seed URLs!");
+    let (time, Some(seed_urls)) = timer.time(|| async { seed_urls::fetch() }).await else {
+        panic!("Failed to fetch seed URLs, exiting...");
     };
-
-    info!("Loaded {} seed URLs!", seed_urls.len());
-
-    // Establish database connection.
-    info!("Connecting to the database...");
-
-    let Ok(url) = get_database_url() else {
-        panic!("Missing environment variables!");
-    };
-    let Ok(pool) = get_pool(&url).await else {
-        panic!("Failed to connect to the database!");
-    };
-
-    info!("Successfully connected to the database!");
+    info!(
+        "Loaded {} seed URLs in {}!",
+        seed_urls.len(),
+        format_time(&time)
+    );
 
     // Crawl seed URLs.
     info!("Crawling seed URLs...");
 
-    for url in seed_urls {
-        info!("Crawling {url}...");
+    let mut websites = Vec::new();
 
-        let mut website: Website = Website::new(&url);
-        website
-            .with_respect_robots_txt(true)
-            .with_subdomains(true)
-            .with_tld(false)
-            .with_delay(0)
-            .with_request_timeout(None)
-            .with_http2_prior_knowledge(false)
-            .with_user_agent(Some("RSE Crawler"))
-            .with_budget(Some(spider::hashbrown::HashMap::from([
-                ("*", 300),
-                ("/licenses", 10),
-            ])))
-            .with_external_domains(Some(
-                Vec::from(
-                    ["https://creativecommons.org/licenses/by/3.0/"]
-                        .map(std::string::ToString::to_string),
-                )
-                .into_iter(),
-            ))
-            .with_headers(None)
-            .with_blacklist_url(Some(Vec::from([
-                "https://choosealicense.com/licenses/".into()
-            ])))
-            .with_proxies(None);
+    let chunks = seed_urls
+        .chunks(utils::get_worker_threads())
+        .map(|chunk| chunk.join("\n"));
+    for chunk in chunks {
+        let chunk_websites = match tokio::spawn(async move {
+            let mut websites = Vec::new();
+            for url in chunk.split('\n') {
+                let website = match crawl(url).await {
+                    Ok(website) => website,
+                    Err(e) => {
+                        error!("Failed to crawl seed URL {url}! (Error: {e})");
 
-        let start = Instant::now();
-        website.scrape().await;
-        let duration = start.elapsed();
+                        continue;
+                    }
+                };
 
-        let Some(pages) = website.get_pages() else {
-            warn!("No pages found for {url}!");
+                info!("Crawled seed URL {url}!");
 
-            continue;
+                websites.push(website);
+            }
+
+            websites
+        })
+        .await
+        {
+            Ok(websites) => websites,
+            Err(e) => {
+                error!("Failed to crawl seed URLs (Error: {e})");
+
+                continue;
+            }
         };
 
-        info!(
-            "Scraping {url} took {:?}, found {:?} total pages.",
-            duration, pages
-        );
-
-        let Ok(mut conn) = pool.get_conn().await else {
-            error!("Failed to get a connection to the database!");
-
-            continue;
-        };
-
-        info!("Indexing pages...");
-
-        let metadata = index_pages(&mut conn, pages).await;
-
-        info!("Indexed {} pages!", metadata.len());
+        websites.extend(chunk_websites);
     }
+
+    info!(
+        "Crawled {} seed URLs in {}!",
+        websites.len(),
+        format_time(&timer.total_time())
+    );
+
+    // Index crawled websites.
+    info!("Indexing crawled websites...");
+
+    let (time, indexed_pages) = timer.time(|| async {
+            info!("Indexing crawled websites...");
+
+            let mut indexed_pages = HashMap::new();
+            for website in websites {
+                let pages = indexer::scrape(&website);
+                for url in pages.keys() {
+                    info!("Indexed page {url}!");
+
+                    indexed_pages.insert(url.to_string(), website.clone());
+
+                    info!("Data: {:#?}", pages.get(url).unwrap());
+
+                    // TODO: Store pages in database.
+                }
+            }
+
+            indexed_pages
+        }).await;
+
+    info!(
+        "Indexed {} pages in {}!",
+        indexed_pages.len(),
+        format_time(&time)
+    );
 }
