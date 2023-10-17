5f069195d48a0fe21dd761ed7d4eafb0b218cd7d
Bastian Asmussen <bastianasmussen@tutanota.com>
2023-10-17 17:27:38 +0200

    Added query handler.
    Added a bit of boilerplate for handling queries.

--- server/rse_server/src/main.rs
+++ server/rse_server/src/main.rs
@@ -1,47 +1,22 @@
+mod query;
 mod utils;
 
-use actix_web::get;
-use actix_web::web::Path;
+use crate::query::Query;
 use actix_web::App;
-use actix_web::HttpResponse;
 use actix_web::HttpServer;
 use actix_web::Responder;
-use log::{error, info};
-
-use utils::db_manager::get_pool;
+use actix_web::{get, web};
 
 #[get("/")]
-async fn root() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World!")
-}
-
-#[get("/query/{query}")]
-async fn query(path: Path<String>) -> impl Responder {
-    let Ok(pool) = get_pool().await else {
-        error!("Failed to connect to the database!");
-
-        return HttpResponse::InternalServerError().body("Failed to connect to the database!");
-    };
-
-    let Ok(_conn) = pool.get_conn().await else {
-        error!("Failed to get a connection from the pool!");
-
-        return HttpResponse::InternalServerError().body("Failed to connect to the database!");
-    };
-
-    info!("Successfully connected to the database!");
-
-    let query = path.into_inner();
-    info!("Query: {}", query);
-
-    HttpResponse::Ok().body("Hello, World!")
+async fn handle_query(query: web::Query<Query>) -> impl Responder {
+    format!("Hello, {}!", query.text)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
-    HttpServer::new(|| App::new().service(root))
+    HttpServer::new(|| App::new().service(handle_query))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
