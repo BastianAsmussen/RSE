d827a0ec791ed586d90d4addc50efe256f0df9ed
BastianAsmussen <bastian@asmsusen.tech>
2023-10-20 12:46:56 +0200

    Update indexer.
    Added a few features, and fixed a bug in creating keywords.
    - Added keyword stemming, stop-word removal and frequency counting.

--- server/rse_crawler/src/indexer.rs
+++ server/rse_crawler/src/indexer.rs
@@ -2,41 +2,6 @@ use crate::utils::db::model::{ForwardLink, Keyword, Page};
 use crate::utils::db::schema::pages::dsl::pages;
 use diesel_async::{AsyncMysqlConnection, RunQueryDsl};
 
-/// Create a new website in the database.
-///
-/// # Arguments
-///
-/// * `page`: The page to create.
-/// * `keywords`: The keywords for the page.
-/// * `forward_links`: The forward links for the page.
-///
-/// # Returns
-///
-/// * `Ok(())` if the website was created successfully, otherwise an `Err`.
-pub async fn new_website(
-    page: &Page,
-    keywords: &Option<Vec<Keyword>>,
-    forward_links: &Option<Vec<ForwardLink>>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut conn = crate::utils::env::database::establish_connection().await?;
-
-    create_page(&mut conn, page).await?;
-
-    if let Some(keywords) = keywords {
-        for keyword in keywords {
-            create_keyword(&mut conn, &keyword).await?;
-        }
-    }
-
-    if let Some(forward_links) = forward_links {
-        for forward_link in forward_links {
-            create_forward_link(&mut conn, &forward_link).await?;
-        }
-    }
-
-    Ok(())
-}
-
 /// Create a page in the database.
 ///
 /// # Arguments
