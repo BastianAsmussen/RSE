64aa0fbfc51ffc3993c63fa4efb37299c23c2619
BastianAsmussen <bastian@asmsusen.tech>
2023-10-26 12:19:17 +0200

    Refactor `create_metadata`.
    Refactored SQL script for creating `metadata` expanding how long `name` and `content` is (`512 -> 1024`).

--- server/database/lib/migrations/2023-10-25-112530_create_metadata/up.sql
+++ server/database/lib/migrations/2023-10-25-112530_create_metadata/up.sql
@@ -2,10 +2,10 @@
 CREATE TABLE metadata
 (
     id      SERIAL PRIMARY KEY,
-    page_id INT          NOT NULL,
+    page_id INT           NOT NULL,
 
-    name    VARCHAR(512) NOT NULL,
-    content VARCHAR(512) NOT NULL,
+    name    VARCHAR(1024) NOT NULL,
+    content VARCHAR(1024) NOT NULL,
 
     FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
 )
