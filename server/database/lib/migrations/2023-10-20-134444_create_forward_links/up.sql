1433a31f339b6bcd6d6975bc416d8579d237198d
BastianAsmussen <bastian@asmsusen.tech>
2023-10-26 12:18:23 +0200

    Refactor `create_forward_links`.
    Refactored SQL script for creating `forward_links` by making its PK be a combination of `from_page_id` and `to_page_id`.

--- server/database/lib/migrations/2023-10-20-134444_create_forward_links/up.sql
+++ server/database/lib/migrations/2023-10-20-134444_create_forward_links/up.sql
@@ -1,9 +1,14 @@
--- Your SQL goes here
 CREATE TABLE forward_links
 (
-    id      SERIAL PRIMARY KEY,
-    page_id INT           NOT NULL,
-    url     VARCHAR(2048) NOT NULL,
+    from_page_id INT NOT NULL,           -- The ID of the page that contains the link.
+    to_page_id   INT NOT NULL,           -- The ID of the page that the link points to.
 
-    FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
+    frequency    INT NOT NULL DEFAULT 1, -- The number of times the link appears on the page.
+
+    -- CHECK (from_page_id <> to_page_id),  -- A page cannot link to itself.
+
+    PRIMARY KEY (from_page_id, to_page_id),
+
+    FOREIGN KEY (from_page_id) REFERENCES pages (id) ON DELETE CASCADE,
+    FOREIGN KEY (to_page_id) REFERENCES pages (id) ON DELETE CASCADE
 )
