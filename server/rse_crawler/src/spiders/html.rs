e2bddbde40bda35e50704e8c6b6234136b955afe
BastianAsmussen <bastian@asmsusen.tech>
2023-10-27 12:46:04 +0200

    Half implemented `keywords` fetching for scraper.

--- server/rse_crawler/src/spiders/html.rs
+++ server/rse_crawler/src/spiders/html.rs
@@ -5,9 +5,11 @@ use db::model::NewMetadata;
 use log::info;
 use regex::Regex;
 use reqwest::{Client, Url};
+use scraper::Html;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::time::Duration;
+use rust_stemmers::Stemmer;
 
 /// A spider that crawls a website.
 ///
@@ -56,11 +58,13 @@ impl WebSpider {
 ///
 /// * `url` - The URL of the website.
 /// * `metadata` - The metadata of the website.
+/// * `keywords` - The keywords of the website.
 /// * `forward_links` - The forward links of the website.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WebItem {
     url: Url,
     metadata: HashMap<String, String>,
+    keywords: HashMap<String, i32>,
     forward_links: Vec<Url>,
 }
 
@@ -99,10 +103,12 @@ impl super::Spider for WebSpider {
         }
 
         // let urls = filter_non_scrapable(urls).await?;
+        let document = Html::parse_document(&html);
 
         let items = vec![WebItem {
             url: url.clone(),
-            metadata: get_metadata(&html)?,
+            metadata: get_metadata(&document)?,
+            keywords: get_keywords(&document)?,
             forward_links: urls.clone(),
         }];
 
@@ -115,9 +121,9 @@ impl super::Spider for WebSpider {
 
         let mut conn = db::get_connection().await?;
 
+        info!("Inserting page...");
         let page = db::create_page(&mut conn, &item.url).await?;
 
-        info!("Inserted page...");
         let metadata = item
             .metadata
             .iter()
@@ -131,6 +137,35 @@ impl super::Spider for WebSpider {
 
         info!("Inserting {} metadata tags...", metadata.len());
         db::create_metadata(&mut conn, &metadata).await?;
+
+        info!("Inserting {} keywords...", item.keywords.len());
+
+        item.keywords
+            .iter()
+            .max_by(|a, b| a.0.len().cmp(&b.0.len()))
+            .map_or_else(
+                || {
+                    info!("No keywords found!");
+                },
+                |keyword| {
+                    info!("Longest Keyword:");
+                    info!("- Word:\t{}", keyword.0);
+                    info!("- Length:\t{}", keyword.0.len());
+                    info!("- Frequency:\t{}", keyword.1);
+                },
+            );
+
+        let keywords = item
+            .keywords
+            .iter()
+            .map(|keyword| db::model::NewKeyword {
+                page_id: page.id,
+
+                word: keyword.0.to_string(),
+                frequency: *keyword.1,
+            })
+            .collect::<Vec<db::model::NewKeyword>>();
+        db::create_keywords(&mut conn, &keywords).await?;
 
         // Count how many times each forward link appears, and add them to the HashMap.
         let forward_links =
@@ -245,15 +280,14 @@ fn normalize_url(origin_url: &Url, to_normalize: &str) -> Result<Url, Error> {
 ///
 /// # Arguments
 ///
-/// * `html` - The HTML.
+/// * `document` - The HTML document.
 ///
 /// # Returns
 ///
 /// * `Ok(HashMap<String, String>)` - The metadata.
 /// * `Err(Error)` - The error.
-fn get_metadata(html: &str) -> Result<HashMap<String, String>, super::Error> {
-    let document = scraper::Html::parse_document(html);
-
+#[allow(clippy::expect_used)]
+fn get_metadata(document: &Html) -> Result<HashMap<String, String>, super::Error> {
     let selector = scraper::Selector::parse("meta")?;
     let meta = document.select(&selector);
     let mut meta_map = HashMap::new();
@@ -269,5 +303,188 @@ fn get_metadata(html: &str) -> Result<HashMap<String, String>, super::Error> {
         meta_map.insert(name.to_string(), content.to_string());
     }
 
+    // Get the title of the page.
+    let selector = scraper::Selector::parse("title")?;
+    let title = document.select(&selector).next();
+    if let Some(title) = title {
+        let title = title.text().collect::<Vec<_>>().join(" ");
+
+        meta_map.insert("title".to_string(), title);
+    }
+
+    // Get the language of the page.
+    let selector = scraper::Selector::parse("html")?;
+    let language = document.select(&selector).next();
+    if let Some(language) = language {
+        let value = language.value().attr("lang").unwrap_or("en");
+
+        meta_map.insert("language".to_string(), value.to_string());
+    };
+
     Ok(meta_map)
+}
+
+/// Extracts the keywords from the HTML.
+///
+/// # Arguments
+///
+/// * `document` - The HTML document.
+///
+/// # Returns
+///
+/// * `HashMap<String, i32>` - The keywords.
+#[allow(clippy::expect_used)]
+fn get_keywords(document: &Html) -> Result<HashMap<String, i32>, super::Error> {
+    // Get the language of the page or default to English.
+    let language = document.root_element().value().attr("lang").unwrap_or("en");
+
+    // What counts as a keyword? Any word that is not a stop word, and is a purely alphabetical word, or purely numeric word.
+    // Grab all the words in the page, filter out tags, and other garbage, and count how many times they appear.
+    let selector = scraper::Selector::parse("body")?;
+    let Some(body) = document.select(&selector).next() else {
+        return Err(super::Error::Scraper("No body found!".to_string()));
+    };
+
+    let words = body
+        .text()
+        .flat_map(|words| words.split_whitespace().collect::<Vec<_>>())
+        .map(str::to_lowercase)
+        .map(|word| {
+            let mut word = word;
+
+            word.retain(|character| character.is_alphabetic() || character.is_numeric());
+            word
+        })
+        .filter(|word| !word.is_empty())
+        .collect::<Vec<_>>();
+
+    let stop_words = get_stop_words();
+    let stemmer = Stemmer::create(determine_stemmer_algorithm(language));
+
+    let keywords = words
+        .iter()
+        .filter(|word| !stop_words.contains(word))
+        .map(|word| {
+            let stemmed_word = stemmer.stem(word);
+
+            stemmed_word.to_string()
+        })
+        .fold(HashMap::new(), |mut map, word| {
+            if let std::collections::hash_map::Entry::Vacant(e) = map.entry(word.clone()) {
+                e.insert(1);
+            } else {
+                let frequency = map.get_mut(&word).expect("Failed to get keyword frequency!");
+
+                *frequency += 1;
+            }
+
+            map
+        });
+
+    Ok(keywords)
+}
+
+/// Gets the stop words.
+///
+/// # Returns
+///
+/// * `Vec<String>` - The stop words.
+///
+/// # Panics
+///
+/// * If the stop words file could not be read.
+#[allow(clippy::expect_used)]
+fn get_stop_words() -> Vec<String> {
+    let stop_words =
+        std::fs::read_to_string("stop_words.txt").expect("Failed to read stop words file!");
+
+    stop_words
+        .split('\n')
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+}
+
+/// Determines the stemmer algorithm to use.
+///
+/// # Arguments
+///
+/// * `language` - The language of the page.
+///
+/// # Returns
+///
+/// * `Algorithm` - The stemmer algorithm.
+///
+/// # Notes
+///
+/// * If the language is not supported, the English stemmer algorithm is used.
+fn determine_stemmer_algorithm(language: &str) -> rust_stemmers::Algorithm {
+    // Determine the language of the page and choose the stemmer accordingly.
+    match language {
+        "ar" => rust_stemmers::Algorithm::Arabic,
+        "da" => rust_stemmers::Algorithm::Danish,
+        "nl" => rust_stemmers::Algorithm::Dutch,
+        "fi" => rust_stemmers::Algorithm::Finnish,
+        "fr" => rust_stemmers::Algorithm::French,
+        "de" => rust_stemmers::Algorithm::German,
+        "el" => rust_stemmers::Algorithm::Greek,
+        "hu" => rust_stemmers::Algorithm::Hungarian,
+        "it" => rust_stemmers::Algorithm::Italian,
+        "no" => rust_stemmers::Algorithm::Norwegian,
+        "pt" => rust_stemmers::Algorithm::Portuguese,
+        "ro" => rust_stemmers::Algorithm::Romanian,
+        "ru" => rust_stemmers::Algorithm::Russian,
+        "es" => rust_stemmers::Algorithm::Spanish,
+        "sv" => rust_stemmers::Algorithm::Swedish,
+        "ta" => rust_stemmers::Algorithm::Tamil,
+        "tr" => rust_stemmers::Algorithm::Turkish,
+        _ => rust_stemmers::Algorithm::English,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_keywords() {
+        let html = r#"
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="description" content="This is a description.">
+                    <meta name="keywords" content="this, is, a, keyword, list">
+                    <meta name="author" content="John Doe">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>LinkedIn</title>
+                </head>
+                <body>
+                    <h1>LinkedIn</h1>
+                    <p>500 million+ members | Manage your professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities.</p>
+                </body>
+            </html>
+        "#;
+
+        let document = Html::parse_document(html);
+
+        let keywords = get_keywords(&document).expect("Failed to get keywords!");
+        let length = keywords.len();
+
+        assert_eq!(length, 14);
+
+        assert_eq!(keywords.get("ident"), Some(&1));
+        assert_eq!(keywords.get("knowledg"), Some(&1));
+        assert_eq!(keywords.get("linkedin"), Some(&1));
+        assert_eq!(keywords.get("500"), Some(&1));
+        assert_eq!(keywords.get("access"), Some(&1));
+        assert_eq!(keywords.get("network"), Some(&1));
+        assert_eq!(keywords.get("insight"), Some(&1));
+        assert_eq!(keywords.get("opportun"), Some(&1));
+        assert_eq!(keywords.get("million"), Some(&1));
+        assert_eq!(keywords.get("profession"), Some(&2));
+        assert_eq!(keywords.get("manag"), Some(&1));
+        assert_eq!(keywords.get("engag"), Some(&1));
+        assert_eq!(keywords.get("member"), Some(&1));
+        assert_eq!(keywords.get("build"), Some(&1));
+    }
 }
