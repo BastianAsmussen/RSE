41afc6c34c304cb392f23604a4e1f14eda215c2c
BastianAsmussen <bastian@asmsusen.tech>
2023-11-02 07:48:55 +0100

    Update logging format.

--- server/rse_crawler/src/main.rs
+++ server/rse_crawler/src/main.rs
@@ -14,11 +14,11 @@ async fn main() {
         utils::env::workers::get_crawlers(),
         utils::env::workers::get_processors(),
     );
-
     let spider = Arc::new(Web::new(
         &utils::env::spider::get_http_timeout(),
         &utils::env::spider::get_user_agent(),
         utils::env::spider::get_word_boundaries(),
     ));
+
     crawler.run(spider).await;
 }
