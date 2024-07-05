8495789db526ad7839cf008779933f4def0fa040
Bastian Asmussen <bastian@asmussen.tech>
2024-07-05 11:57:39 +0200

    chore: improve formatting in SQL migration file for better readability

--- crawler/migrations/2024-07-05-072532_create_websites/up.sql
+++ crawler/migrations/2024-07-05-072532_create_websites/up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE websites
 (
-    id SERIAL PRIMARY KEY,
+    id              SERIAL PRIMARY KEY,
 
-    url VARCHAR(8192) NOT NULL UNIQUE,
-    last_crawled_at TIMESTAMP NOT NULL DEFAULT NOW()
+    url             VARCHAR(8192) NOT NULL UNIQUE,
+    last_crawled_at TIMESTAMP     NOT NULL DEFAULT now()
 );
 
-CREATE INDEX url_idx ON websites(url);
+CREATE INDEX url_idx ON websites (url);
