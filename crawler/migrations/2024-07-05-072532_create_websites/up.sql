33482a7e7fb5eb53e62b05455685b667739be148
Bastian Asmussen <bastian@asmussen.tech>
2024-07-05 11:48:19 +0200

    chore: add database integration with Diesel and initial migrations
    - Integrated Diesel ORM for database management.
    - Added initial database migrations for setting up the  table.
    - Configured environment variables for database connection.
    - Updated dependencies to include Diesel, Diesel-async, and related crates.
    - Implemented database initialization and migration handling in  module.

---
+++ crawler/migrations/2024-07-05-072532_create_websites/up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE websites
+(
+    id SERIAL PRIMARY KEY,
+
+    url VARCHAR(8192) NOT NULL UNIQUE,
+    last_crawled_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX url_idx ON websites(url);
--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,46 +1,15 @@
 mod crawler;
 
-use std::collections::HashSet;
-
 use anyhow::Result;
-use crawler::Crawler;
-use reqwest::Url;
+
+mod db;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (thread_sender, thread_receiver) = std::sync::mpsc::channel();
-    tokio::spawn(async move {
-        loop {
-            let handle = thread_receiver
-                .recv()
-                .expect("Failed to read from channel!");
-            if let Err(why) = handle.await {
-                eprintln!("An error occurred: {why}");
-            };
-        }
-    });
+    dotenvy::dotenv().ok();
+    tracing_subscriber::fmt::init();
 
-    let (url_sender, url_receiver) = std::sync::mpsc::channel();
-    url_sender.send(Url::parse("https://en.wikipedia.org/wiki/Main_Page")?)?;
+    let db = db::init().await.expect("Failed to connect to database!");
 
-    let mut seen_urls = HashSet::new();
-    loop {
-        let next_url = url_receiver.recv()?;
-        println!("Found '{}'.", next_url.as_str());
-
-        if seen_urls.contains(&next_url) {
-            continue;
-        }
-
-        let url_sender = url_sender.clone();
-        seen_urls.insert(next_url.clone());
-
-        let handle = tokio::spawn(async move {
-            let mut crawler = Crawler::new(url_sender, next_url);
-
-            crawler.crawl().await
-        });
-
-        thread_sender.send(handle)?;
-    }
+    Ok(())
 }
