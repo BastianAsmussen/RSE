64d0522be2ca8c456ba4fe50227f9f7794fd0b71
Bastian Asmussen <bastian@asmussen.tech>
2024-07-10 22:58:38 +0200

    chore: add new dependencies for tailwindcss and flowbite integration
    Added @catppuccin/tailwindcss, flowbite, and flowbite-svelte to enhance the styling capabilities. Updated the Tailwind CSS configuration to include these plugins and their respective content paths.

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,10 +1,1 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
-
+<h1 class="bg-base text-pink">Hello world!</h1>
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,9 +1,19 @@
+import flowbitePlugin from 'flowbite/plugin';
+import catppuccinPlugin from '@catppuccin/tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		catppuccinPlugin({
+			defaultFlavour: 'mocha'
+		}),
+		flowbitePlugin
+	]
+};
