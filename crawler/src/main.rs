23c658728977ed9f35ee0f5c8754d1fad9c85d58
Bastian Asmussen <bastian@asmussen.tech>
2024-07-12 04:50:56 +0200

    feat: add website crawling functionality with cache integration
    Enhanced the crawler to fetch websites from the cache and add new websites to the crawl list. Integrated tracing for better logging and error handling.

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,4 +1,8 @@
+use std::sync::mpsc::channel;
+
 use anyhow::Result;
+use crawler::Crawler;
+use tracing::{info, warn};
 
 mod db;
 mod cache;
@@ -10,10 +14,30 @@ async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let db = db::init().await.expect("Failed to connect to database!");
-    let cache = cache::init().expect("Failed to connect to cache!");
+    let db = db::init().await?;
+    let cache = cache::init()?;
 
-    println!("Connected to databases!");
+    let (sender, reciever) = channel();
+    let mut conn = cache.get_connection()?;
+    tokio::spawn(async move {
+        loop {
+            let Ok(url) = reciever.recv() else {
+                continue;
+            };
+
+            match cache::add_website(&mut conn, &url) {
+                Ok(()) => info!("Added {url} to crawl list."),
+                Err(why) => warn!("Failed to add {url} to crawl list: {why}"),
+            };
+        }
+    });
+
+    info!("Connected to databases!");
+    while let Some(url) = cache::next_website(&mut cache.get_connection()?)? {
+        info!("crawlin...");
+        let mut crawler = Crawler::new(sender.clone(), url);
+        crawler.crawl().await?;
+    }
 
     Ok(())
 }
